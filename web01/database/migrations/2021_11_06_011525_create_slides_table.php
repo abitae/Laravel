a299db9e7322f5c219e2114940703b9c0522490d
abitae <abel.arana@hotmail.com>
2021-11-05 20:17:07 -0500

    actualizacion

---
+++ web01/database/migrations/2021_11_06_011525_create_slides_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('slides', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id');
+            $table->string('ip_address', 45)->nullable();
+            $table->string('user_agent')->nullable();
+            $table->string('payload');
+            $table->string('last_activity')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('slides');
+    }
+}
